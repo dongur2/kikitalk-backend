432d105fe90f4918b73ff5a45c8c369b071f34e1
dongur2 <holymolyguri@gmail.com>
2025-02-06 20:42:33 +0900

    :art: Refactor: OAuth2 로그인 시 미가입 사용자 예외 처리 로직 추가 및 리팩토링
    - 미가입 사용자일 경우 발생하는 `NullPointerException`을 `catch` 블록에서 처리하도록 수정
    - `catch` 블록에서 미가입 사용자 정보 바인딩 및 회원가입 폼으로 리다이렉트되도록 구조 수정
    - 미가입 사용자의 정보를 쿠키에 저장하는 `bindUserInfoFromOAuthOnCookie` 메서드 분리

--- src/main/java/com/kikitalk/chatting/global/security/oauth2/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/kikitalk/chatting/global/security/oauth2/handler/OAuth2SuccessHandler.java
@@ -36,30 +36,24 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         }
 
         //카카오 서버로부터 받아올 수 있는 닉네임, 프사URL 받아왔으므로
-        //DB에 저장된 회원인지 확인하고
-        User dbUser = userService.getUserBySnsId(principal.getUserInfo().getId());
+        try {
+            //DB에 저장된 회원인지 확인
+            User dbUser = userService.getUserBySnsId(principal.getUserInfo().getId());
 
-        //없는 회원이면 DB 저장 - 회원가입 진행
-        if(dbUser == null) {
-            log.info("미가입 회원입니다.");
-            Cookie cookie = new Cookie("id", principal.getUserInfo().getId());
-            cookie.setAttribute("nickname", principal.getUserInfo().getNickname());
-            cookie.setAttribute("pic", principal.getUserInfo().getProfileImageUrl());
+            //있는 회원이면 로그인 진행: 토큰 발급
+            String accessToken = userService.signIn(dbUser);
+            log.info("액세스 토큰 발급: {}", accessToken);
 
-            cookie.setHttpOnly(true);
-            cookie.setMaxAge(60 * 5); //5분
+            //헤더에 토큰 전달
+            response.setHeader("Authorization", accessToken);
+
+        } catch (NullPointerException e) {
+            //없는 회원이면 회원가입 진행: OAuth2로 받아온 데이터 전달
+            bindUserInfoFromOAuthOnCookie(principal);
 
             //회원가입 폼으로
             return UriComponentsBuilder.fromUriString("http://localhost:8080/api/v1/users/register").build().toUriString();
         }
-
-        //있는 회원이면 로그인 진행 - 토큰 발급
-        log.info("가입 회원입니다.");
-        String accessToken = userService.signIn(dbUser);
-        log.info("액세스 토큰 발급 완료: {}", accessToken);
-
-        //헤더에 토큰 전달
-        response.setHeader("Authorization", accessToken);
 
         return UriComponentsBuilder.fromUriString(LOGIN_URL).build().toUriString();
     }
@@ -67,5 +61,14 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private OAuth2UserPrincipal getOAuth2UserPrincipal(Authentication authentication) {
         Object principal = authentication.getPrincipal();
         return (principal instanceof OAuth2UserPrincipal) ? (OAuth2UserPrincipal) principal : null;
+    }
+
+    private void bindUserInfoFromOAuthOnCookie(OAuth2UserPrincipal principal) {
+        Cookie cookie = new Cookie("id", principal.getUserInfo().getId());
+        cookie.setAttribute("nickname", principal.getUserInfo().getNickname());
+        cookie.setAttribute("pic", principal.getUserInfo().getProfileImageUrl());
+
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(60 * 5); //5분
     }
 }
